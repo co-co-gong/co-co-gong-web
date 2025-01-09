d60271282f6965de4b95d956d9f5ec0c9b994b2c
rldnd <gi981226@gmail.com>
2025-01-10 02:05:17 +0900

    feat(project-setting): stylelint, lint-staged, commitlint
    Resolved: #1

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,11 @@
+import path from "path";
+
+const getFiles = (filenames) => filenames.map((f) => path.relative(process.cwd(), f));
+
+export default {
+  "*.{ts,tsx}": (filenames) => [
+    `prettier --w ${getFiles(filenames).join(" ")} --ignore-unknown --check`,
+    `next lint --fix --file ${getFiles(filenames).join(" --file ")}`,
+  ],
+  "src/**/*.scss": (filenames) => `stylelint --fix ${getFiles(filenames).join(" ")}`,
+};
