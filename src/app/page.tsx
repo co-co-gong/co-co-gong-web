2b9f928076c8f5e37f3b8c56a806fb76f9399be0
rldnd <gi981226@gmail.com>
2025-01-09 23:50:39 +0900

    feat(project-setting): eslint config
    Related to: #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
@@ -19,7 +20,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className={styles.ctas}>
           <a
             className={styles.primary}
