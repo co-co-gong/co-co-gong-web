7cf80ed1025ad90a6fb3e36a11ac9507239e3167
rldnd <gi981226@gmail.com>
2025-01-22 20:41:31 +0900

    fix(data-fetching-modules): render useQuery on homePage to check cors error
    Related to: #4

--- src/entities/user/api/user.query.ts
+++ src/entities/user/api/user.query.ts
@@ -1,10 +1,15 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
-import { apiClient } from "@/shared/api";
+import { apiClient, ApiResponseDTO } from "@/shared/api";
+
+interface User {
+  username: string;
+  email: string;
+}
 
 export const userQueries = createQueryKeys("users", {
   getUsers: (userId: number) => ({
     queryKey: ["user", userId],
-    queryFn: () => apiClient.get(`api/users/rldnd/${userId}`),
+    queryFn: async () => apiClient.get<ApiResponseDTO<User>>(`api/users/rldnd/${userId}`).json(),
   }),
 });
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
-import { getUserApi } from "@/entities/user/api/user.server-service";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { userQueries } from "@/entities/user/api/user.query";
 
 import styles from "./homePage.module.scss";
 
-const HomePage: React.FC = async () => {
-  const user = await getUserApi(1);
+const HomePage: React.FC = () => {
+  const { data } = useQuery(userQueries.getUsers(1));
 
-  return (
-    <div className={styles.wrapper}>
-      HOME PAGE 이메일: {user.data.email} / 이름: {user.data.username}
-    </div>
-  );
+  return <div className={styles.wrapper}>HOME PAGE {JSON.stringify(data)}</div>;
 };
 
 export default HomePage;
