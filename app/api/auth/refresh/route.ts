95ba080d55e30ca900df6c34622de14b6fb82e0c
rldnd <gi981226@gmail.com>
2025-01-31 15:47:29 +0900

    refactor(auth): authorization, refresh logic on server side
    Related to: #9

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,15 +1,12 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { ApiResponseDTO, apiServer } from "@/shared/api";
 import type { TokenDTO } from "@/shared/api/auth";
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/shared/constants/storage";
 import { isFetchError } from "@/shared/lib";
+import { getTokens, removeTokens, setTokens } from "@/shared/lib/auth";
 
 export async function POST() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get(ACCESS_TOKEN_KEY)?.value;
-  const refreshToken = cookieStore.get(REFRESH_TOKEN_KEY)?.value;
+  const { accessToken, refreshToken } = await getTokens();
 
   if (!refreshToken || !accessToken) return NextResponse.error();
 
@@ -18,14 +15,12 @@ export async function POST() {
   });
 
   if (isFetchError(response)) {
-    cookieStore.delete(ACCESS_TOKEN_KEY);
-    cookieStore.delete(REFRESH_TOKEN_KEY);
+    await removeTokens();
     return NextResponse.error();
   }
 
   const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data;
-  cookieStore.set(ACCESS_TOKEN_KEY, newAccessToken);
-  cookieStore.set(REFRESH_TOKEN_KEY, newRefreshToken);
+  await setTokens({ accessToken: newAccessToken, refreshToken: newRefreshToken });
 
   return NextResponse.json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
 }
